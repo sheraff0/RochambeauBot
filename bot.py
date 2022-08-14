5dbb58660e49b0029c363de7ed96a0ee06265c59
radimir <radimir.shevchenko@gmail.com>
2022-08-15 00:34:34 +0300

    Simple config

--- bot.py
+++ bot.py
@@ -1,18 +1,21 @@
-import datetime
+import asyncio
+from dotenv import dotenv_values
+
 from telegram import (
     Bot, Update, ReplyKeyboardMarkup,
     InlineKeyboardButton, InlineKeyboardMarkup
 )
 from telegram.ext import (
+    ApplicationBuilder, ContextTypes,
     MessageHandler, ConversationHandler, CommandHandler,
-    Updater, Filters, CallbackContext, CallbackQueryHandler,
+    Updater, CallbackContext, CallbackQueryHandler,
 )
-from telegram.utils.request import Request
+from telegram.ext.filters import TEXT
 from telegram.error import BadRequest
 
-from django.conf import settings as _
-from bot.models import Profile, Message
-from .game import Roshambo, FIGURES_LIST
+from game import Roshambo, FIGURES_LIST
+
+config = dotenv_values(".env")
 
 
 class RoshamboMixin:
@@ -25,30 +28,35 @@ class RoshamboMixin:
         ) for key, value in enumerate(list_)]
 
     def score(self, list_):
-        return ':'.join(map(str, list_))
+        return ' : '.join(map(str, list_))
 
     def tableau(self):
         return '\n'.join([
             f"Счёт в текущем раунде - {self.score(self.game.attempts_score)}",
             f"Счёт по раундам - {self.score(self.game.rounds_score)}",
-            *([f"Вы {self.game.player_move or ''} -> <- {self.game.opponent_move or ''} Компьютер"] if self.game.player_move else []),
+            *([f"Вы ~ {self.game.player_move or ''} => <= {self.game.opponent_move or ''} ~ Компьютер"] if self.game.player_move else []),
             *([f"""В {self.game.rounds_count} раунде {
                 'победили Вы' if self.game.round_winner == 0 else 'победил компьютер'
             }."""] if self.game.round_winner is not None else []),
             *([] if self.game.game_winner is not None else ["Ваш следующй ход:"]),
         ])
 
-    def start(self, update: Updater, context: CallbackContext) -> int:
+    async def start(self, update: Updater, context: CallbackContext) -> int:
+        user = update.effective_user
+        user_name = user.first_name
         reply_keyboard = [self.options_list_buttons(self.rounds)]
         reply_markup = InlineKeyboardMarkup(reply_keyboard)
-        update.message.reply_text(
-            "Максимальный счёт по раундам", reply_markup=reply_markup)
+        await update.message.reply_text(
+            f'''Привет, {user_name}!
+Сыграй в "Камень-ножницы-бумага" с ботом!
+Выбери максимальный счёт по раундам:''',
+            reply_markup=reply_markup)
         self.game = None
         return self.NEXT_MOVE
 
-    def next_move(self, update: Update, context: CallbackContext) -> int:
+    async def next_move(self, update: Update, context: CallbackContext) -> int:
         query = update.callback_query
-        query.answer()
+        await query.answer()
         move = None
         if self.game:
             move = int(query.data)
@@ -62,20 +70,21 @@ class RoshamboMixin:
         reply_keyboard = [self.options_list_buttons(FIGURES_LIST)]
         reply_markup = InlineKeyboardMarkup(reply_keyboard)
         try:
-            query.edit_message_text(
+            await query.edit_message_text(
                 text=self.tableau(),
-                reply_markup=reply_markup
+                reply_markup=reply_markup,
             )
         except BadRequest:
             pass
         return self.NEXT_MOVE
 
-    def game_over(self, update: Update, context: CallbackContext) -> int:
+    async def game_over(self, update: Update, context: CallbackContext) -> int:
         query = update.callback_query
-        query.answer()
-        query.edit_message_text(
+        await query.answer()
+        await query.edit_message_text(
             text=f"""{self.tableau()}
-ИГРА ОКОНЧЕНА! {'Победили Вы' if self.game.game_winner == 0 else 'Победил компьютер'}.""")
+ИГРА ОКОНЧЕНА! {'Победили Вы' if self.game.game_winner == 0 else 'Победил компьютер'}.""",
+        )
         return ConversationHandler.END
 
     def get_handler(self):
@@ -92,30 +101,21 @@ class RoshamboMixin:
 class TelegramBot:
     def __init__(self):
         self.create_bot()
-        self.set_updater()
+        print(self.bot)
+        self.add_handler()
+        self.bot.run_polling()
 
     def create_bot(self):
-        request = Request(
-            connect_timeout=1.5,
-            read_timeout=1.5,
-            con_pool_size=8
-        )
-        self.bot = Bot(
-            request=request,
-            token=_.TELEGRAM_BOT_TOKEN,
-            # base_url=_.TELEGRAM_PROXY_URL,
-        )
+        self.bot = ApplicationBuilder().token(
+            config.get("TOKEN")).build()
 
-    def set_updater(self):
-        updater = Updater(bot=self.bot)
-        updater.dispatcher.add_handler(
-            self.get_handler())
-        updater.start_polling()
-        updater.idle()
+    def add_handler(self):
+        handler = self.get_handler()
+        self.bot.add_handler(handler)
 
     def get_handler(self):
         # Echo as default
-        return MessageHandler(Filters.text, self.do_echo)
+        return MessageHandler(TEXT, self.do_echo)
 
     def get_message_details(self, update):
         return (
@@ -124,11 +124,15 @@ class TelegramBot:
             update.message.from_user
         )
 
-    def do_echo(self, update: Update, context: CallbackContext):
+    async def do_echo(self, update: Update, context: CallbackContext):
         chat_id, text, from_user = self.get_message_details(update)
-        update.message.reply_text(
+        await update.message.reply_text(
             text=f"{chat_id}\n\n{text}\n{from_user.first_name}")
 
 
 class RoshamboBot(RoshamboMixin, TelegramBot):
     pass
+
+
+if __name__ == "__main__":
+    bot = RoshamboBot()
